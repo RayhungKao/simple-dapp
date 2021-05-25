c8dd0f59f4e318191ca7e9a67ac2dd9f3205900d
rayhungkao <ryamkao2339@gmail.com>
2021-05-25 21:58:28 +0800

    Updates

---
+++ precache-manifest.44a0f760cce2cfe4c56081bf1c787d30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0227145902a737aae7f668b60d81208",
+    "url": "/simple-dapp/index.html"
+  },
+  {
+    "revision": "27f25a069066fd5a144a",
+    "url": "/simple-dapp/static/css/main.a3487775.chunk.css"
+  },
+  {
+    "revision": "86179571dd09f016d4d5",
+    "url": "/simple-dapp/static/js/2.7b013f7d.chunk.js"
+  },
+  {
+    "revision": "27f25a069066fd5a144a",
+    "url": "/simple-dapp/static/js/main.c2dcf96f.chunk.js"
+  },
+  {
+    "revision": "6b50e368e1fc88384d32",
+    "url": "/simple-dapp/static/js/runtime-main.f7624dd0.js"
+  }
+]);
